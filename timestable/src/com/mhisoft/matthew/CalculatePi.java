3bc471c92116afa24ec2e456c90867de1f0ebac9
Matthew <matthewjxue@gmail.com>
2018-06-23 11:44:31 -0400

    Made new programs

--- timestable/src/com/mhisoft/matthew/CalculatePi.java
+++ timestable/src/com/mhisoft/matthew/CalculatePi.java
@@ -1,17 +1,4 @@
-/* -----------------------------------------------------------------------------
-* Copyright (c) 2013 SuccessFactors, all rights reserved.
-*
-* This software and documentation is the confidential and proprietary
-* information of SuccessFactors.  SuccessFactors makes no representation 
- * or warranties about the suitability of the software, either expressed or 
- * implied.  It is subject to change without notice.
-*
-* U.S. and international copyright laws protect this material.  No part
-* of this material may be reproduced, published, disclosed, or
-* transmitted in any form or by any means, in whole or in part, without
-* the prior written permission of SuccessFactors.
-* -----------------------------------------------------------------------------
-*/
+
 
 package com.mhisoft.matthew;
 
