0e86223ed4497c9de5fdafdf5f5f5581e8dac489
Matthew <matthewjxue@gmail.com>
2019-06-30 14:42:58 -0400

    New Composition Program

---
+++ timestable/src/com/mhisoft/matthew/Initialization/Insect.java
@@ -0,0 +1,16 @@
+package com.mhisoft.matthew.Initialization;
+
+public class Insect {
+    int i = 9;
+    int j;
+    Insect(){
+        prt("i = " + i +",j=" + j);
+        j=39;
+    }
+    static int x1 =
+            prt("static Insect.x1 initialized");
+                    static int prt(String s){
+                        System.out.println(s);
+                        return 47;
+                    }
+}
